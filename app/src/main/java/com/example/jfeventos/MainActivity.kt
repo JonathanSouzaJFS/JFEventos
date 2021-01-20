758417ebcf85c77b530a0c15d911fa000f10de05
JonathanFeitosa <jfsjonathanfeitosa@gmail.com>
2021-01-20 01:36:18 -0400

    Adding navigation system

--- app/src/main/java/com/example/jfeventos/MainActivity.kt
+++ app/src/main/java/com/example/jfeventos/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.jfeventos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.homeFragment,
+//                R.id.favoritesFragment
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        nav_view.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
