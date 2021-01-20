b982a0b8cce57f92d079cbb5a6f194eae3631167
JonathanFeitosa <jfsjonathanfeitosa@gmail.com>
2021-01-20 03:39:41 -0400

    Adding biding in HomeFragment

---
+++ app/src/main/java/com/example/jfeventos/ui/HomeFragment.kt
@@ -0,0 +1,74 @@
+package com.example.jfeventos.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.jfeventos.databinding.FragmentHomeBinding
+import com.example.jfeventos.utils.NetworkResponse
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
+
+    private val viewModel by viewModel<HomeViewModel>()
+    private lateinit var mBinding: FragmentHomeBinding
+    private var loading = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupRecyclerView()
+        setupRecyclerViewAdapter()
+        viewModel.getEvents(requireActivity())
+        setupObservers()
+        mBinding.swipeRefresh.setOnRefreshListener(this)
+        setHasOptionsMenu(true)
+    }
+
+    private fun setupRecyclerView() {
+        mBinding.recyclerViewEvents.layoutManager = LinearLayoutManager(requireContext())
+        mBinding.recyclerViewEvents.setHasFixedSize(true)
+    }
+
+    private fun setupRecyclerViewAdapter() {
+    }
+
+    private fun setupObservers() {
+        viewModel.getEvents(requireContext()).observe(requireActivity(), Observer {
+            it?.let { resource ->
+                when (resource) {
+                    is NetworkResponse.Success -> {
+                        mBinding.progressBar.visibility = View.GONE
+                        loading = false
+                        mBinding.swipeRefresh.isRefreshing = false
+                    }
+                    is NetworkResponse.Error -> {
+                        mBinding.progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), resource.exception, Toast.LENGTH_LONG)
+                            .show()
+                        loading = false
+                        mBinding.swipeRefresh.isRefreshing = false
+                    }
+                    is NetworkResponse.Loading -> {
+                        mBinding.progressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onRefresh() {
+    }
+}
